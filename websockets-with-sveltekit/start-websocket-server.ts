1c50e7e9e15a480f777b4def7361857846e2415f
Benjamin Knox <bknox.contact@gmail.com>
2025-09-28 16:54:08 -0400

    Update start-websocket-server.ts

--- websockets-with-sveltekit/start-websocket-server.ts
+++ websockets-with-sveltekit/start-websocket-server.ts
@@ -1,7 +1,6 @@
 import { Server } from 'socket.io';
 
 export const startWebsocketServer = async (httpServer : any) => {
-  const { publisher: redis } = await initRedis();
   const io = new Server(httpServer);
 
   io.on('connection', (socket) => {
