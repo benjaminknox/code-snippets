50f52f9fec8db0adcd8ba4f7c99891a70079cff2
Benjamin Knox <bknox.contact@gmail.com>
2025-09-28 18:24:20 -0400

    Create front-end.svelte

---
+++ websockets-with-sveltekit/front-end.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { io } from 'socket.io-client';
+
+  const socket = io();
+
+  let backEndMessage = $state("");
+
+  socket.emit('frontend-message', {frontEnd: `${Date.now()}`});
+  
+  socket.on('backend-message-to-frontend', (data) => {
+    backendMessage = data;
+  });
+</script>
+
+<div>Svelte Frontend: {backendMessage}</div>
