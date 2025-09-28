a6c9409fe565f080a98a306f5c594d5e0401a3c1
Benjamin Knox <bknox.contact@gmail.com>
2025-09-28 16:22:56 -0400

    Create install.sh

---
+++ websockets-with-sveltekit/install.sh
@@ -0,0 +1,13 @@
+# WebSocket package for the backend
+npm i socket.io
+
+# WebSocket package for the frontend
+npm i socket.io-client
+
+# Sveltkit Adapter for Node, used for building our application
+npm i -D @sveltejs/adapter-node
+
+# Optional: express for your server you can fit this to 
+#           your server but the snippets of code will be
+#           shown here using express (for production builds)
+npm i express
