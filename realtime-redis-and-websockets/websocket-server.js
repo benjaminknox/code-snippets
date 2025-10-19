bb4dc2c7a845acc42d26dec163a92d42b9a33fd8
Benjamin Knox <bknox.contact@gmail.com>
2025-10-19 13:57:26 -0400

    Refactor websocket server to use async start function

--- realtime-redis-and-websockets/websocket-server.js
+++ realtime-redis-and-websockets/websocket-server.js
@@ -1,28 +1,29 @@
 import { Server } from 'socket.io';
 import { createClient } from 'redis';
 
-const io = new Server(httpServer);
+export const startWebsocketServer = async (httpServer : any) => {
+  const io = new Server(httpServer);
 
-// The front end establishes a connection
-io.on('connection', (socket) => { 
-  let redisClient;
-
-  socket.on('register', async (channelId) => {
-    // The server joins a room to isolate messages by user
-    socket.join(channelId);
-
-    redisClient = createClient({ url: process.env.REDIS_URI });
-
-    await redisClient.connect();
-    
-    // The server subscribes to a user specific pub/sub channel
-    redisClient.subscribe(`channel:${channelId}`, async (message) => {
-      // Receive from the back end and send a message to the user via the front end
-      socket.emit('front-end-message', message);
+  let redisClient = createClient({ url: process.env.REDIS_URI });
+  
+  await redisClient.connect();
+  
+  // The front end establishes a connection
+  io.on('connection', (socket) => { 
+  
+    socket.on('register', async (channelId) => {
+      // The server joins a room to isolate messages by user
+      socket.join(channelId);
+      
+      // The server subscribes to a user specific pub/sub channel
+      redisClient.subscribe(`channel:${channelId}`, async (message) => {
+        // Receive from the back end and send a message to the user via the front end
+        socket.emit('front-end-message', message);
+      });
+      
+      socket.on('disconnect', async () => {
+        await redisClient.unsubscribe(`channel:${channelId}`);
+      });
     });
   });
-
-  socket.on('disconnect', async () => {
-    await redisClient?.quit();
-  });
-});
+}
