2befa9d92faee56392d65dff19a4d27baa00080f
Benjamin Knox <bknox.contact@gmail.com>
2025-10-26 13:06:22 -0400

    Update Socket.IO Redis adapter package

--- scaling-websockets-with-redis/install.sh
+++ scaling-websockets-with-redis/install.sh
@@ -2,4 +2,4 @@
 npm install redis
 
 # Socket.IO client for WebSockets with Redis Streams adapter
-npm install socket.io socket.io-client @socket.io/redis-streams-adapter
+npm install socket.io socket.io-client @socket.io/redis-adapter
