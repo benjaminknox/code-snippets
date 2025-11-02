672ce8b91c1b5b7326458d5778955cd72eb6d5e0
Benjamin Knox <bknox.contact@gmail.com>
2025-11-02 15:43:11 -0500

    Add OIDC to parameters to K3s installation
    This script installs K3s with Keycloak OIDC configuration.

---
+++ use-keycloak-with-your-k3s-cluster/update-k3s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Replace your keycloak domain, your realm, and your client id.
+
+curl -sfL https://get.k3s.io | sh -s - server \
+  --kube-apiserver-arg=oidc-issuer-url=https://<your keycloak domain>/realms/<your realm> \
+  --kube-apiserver-arg=oidc-client-id=<your client id> \
+  --kube-apiserver-arg=oidc-username-claim=preferred_username \
+  --kube-apiserver-arg=oidc-groups-claim=groups \
+  --kube-apiserver-arg=oidc-username-prefix=oidc: \
+  --kube-apiserver-arg=oidc-groups-prefix=oidc:
