937a931a2404ce3abaedd958e11936ac7229c586
Benjamin Knox <bknox.contact@gmail.com>
2025-11-16 15:23:29 -0500

    Clarify comments in quill-editor.svelte
    Updated comments to clarify future steps for creating the editor and toolbar elements.

---
+++ a-rich-text-editor-in-sveltekit-with-quill/quill-editor.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import 'quill/dist/quill.snow.css';
+
+  let { data } = $props();
+
+  let content: HTMLInputElement; // We will create an input element in step 4
+
+  $effect(() => {
+    let quill;
+
+    (async () => {
+      const Quill = (await import('quill')).default;
+      
+      quill = new Quill('#editor', { // We will create the editor element in step 5
+        theme: 'snow',
+        modules: {
+          toolbar: '#toolbar' // We will create the toolbar element in step 6
+        }
+      });
+      
+      quill.on('text-change', () => {
+        content.value = quill.root.innerHTML;
+      });
+    })();
+    
+    return () => {
+      if (quill) {
+        quill.off('text-change');
+      }
+    };
+  });
+</script>
