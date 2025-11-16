c89df56a306e97ca42efd452c061b3e826acfb9c
Benjamin Knox <bknox.contact@gmail.com>
2025-11-16 16:25:10 -0500

    Add toolbar for rich text editor with Quill

---
+++ a-rich-text-editor-in-sveltekit-with-quill/toolbar.svelte
@@ -0,0 +1,32 @@
+<div id="toolbar">
+  <!-- Add font size dropdown -->
+  <select class="ql-size">
+    <option value="small"></option>
+    <!-- Note a missing, thus falsy value, is used to reset to default -->
+    <option selected></option>
+    <option value="large"></option>
+    <option value="huge"></option>
+  </select>
+  <!-- Add a bold button -->
+  <button class="ql-bold"></button>
+  <!-- Add subscript and superscript buttons -->
+  <button class="ql-script" value="sub"></button>
+  <button class="ql-script" value="super"></button>
+</div>
+
+<div id="editor"></div>
+
+<style>
+  #toolbar {
+    width: 600px;
+    margin: auto;
+    border-radius: 10px 10px 0 0;
+  }
+
+  #editor {
+    width: 600px;
+    height: 300px;
+    margin: auto;
+    border-radius: 0 0 10px 10px;
+  }
+</style>
