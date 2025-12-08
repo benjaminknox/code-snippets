e493cd0de4aa5e0bbc2e3fcca7ce8dc72b9938f3
Benjamin Knox <bknox.contact@gmail.com>
2025-12-07 20:39:15 -0500

    Update placeholders in users-by-id-curl.sh

--- keycloak-users-api-returns-403-and-how-to-fix-it/users-by-id-curl.sh
+++ keycloak-users-api-returns-403-and-how-to-fix-it/users-by-id-curl.sh
@@ -1,8 +1,8 @@
-ACCESS_TOKEN=$(curl -s '<PLACE YOUR KEYCLOAK HOST>/realms/403-forbidden-fix/protocol/openid-connect/token' \
+ACCESS_TOKEN=$(curl -s '<PLACE YOUR KEYCLOAK HOST HERE>/realms/403-forbidden-fix/protocol/openid-connect/token' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'grant_type=client_credentials' \
   -d 'client_id=403-forbidden-fix-client' \
   -d 'client_secret=<PLACE KEYCLOAK CLIENT SECRET HERE>' | grep -o '"access_token":"[^"]*' | sed 's/"access_token":"//')
 
-curl '<PLACE YOUR KEYCLOAK HOST>/admin/realms/403-forbidden-fix/users/<PLACE YOUR USER ID HERE>' \
+curl '<PLACE YOUR KEYCLOAK HOST HERE>/admin/realms/403-forbidden-fix/users/<PLACE YOUR USER ID HERE>' \
   -H "Authorization: Bearer ${ACCESS_TOKEN}"
