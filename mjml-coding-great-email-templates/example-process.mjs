e53f0be771b6c5c9ddc9a88b95fc3c50978ad326
Benjamin Knox <bknox.contact@gmail.com>
2026-01-24 12:39:04 -0500

    Clarify console output with a comment
    Added comment to clarify the output of the console log.

--- mjml-coding-great-email-templates/example-process.mjs
+++ mjml-coding-great-email-templates/example-process.mjs
@@ -4,4 +4,4 @@ import { readFileSync } from 'fs';
 let mjml = readFileSync('./example-email.mjml', 'utf-8');
 let translated = mjml2html(mjml);
 
-console.log(translated.html);
+console.log(translated.html); // Output should be the HTML file above
