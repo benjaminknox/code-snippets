89f112d15bee9f80913c31738b22a3e2a7e83175
Benjamin Knox <bknox.contact@gmail.com>
2026-06-05 09:25:53 -0400

    Update cert-manager webhook deployment port

--- the-cert-manager-fargate-gotcha/the-solution.sh
+++ the-cert-manager-fargate-gotcha/the-solution.sh
@@ -1,3 +1,4 @@
+kubectl delete deployment cert-manager-webhook -n cert-manager && \
 curl -fsSL https://github.com/cert-manager/cert-manager/releases/download/v1.20.2/cert-manager.yaml \
   | sed -e 's/--secure-port=10250/--secure-port=10260/' \
         -e 's/containerPort: 10250/containerPort: 10260/' \
