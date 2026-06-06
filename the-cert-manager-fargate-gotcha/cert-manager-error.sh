2acfcacda0b31c6f23619f5295912f6d31d8e531
Benjamin Knox <bknox.contact@gmail.com>
2026-06-06 17:09:52 -0400

    Update cert-manager-error.sh

--- the-cert-manager-fargate-gotcha/cert-manager-error.sh
+++ the-cert-manager-fargate-gotcha/cert-manager-error.sh
@@ -4,5 +4,4 @@ error: failed to trigger issuance of Certificate <namespace>/<certificate nam>:
 Internal error occurred: failed calling webhook "webhook.cert-manager.io":
 failed to call webhook: Post "https://cert-manager-webhook.cert-manager.svc:443/validate?timeout=30s":
 tls: failed to verify certificate:x509:
-certificate is valid for fargate-ip-172-21-110-185.us-west-2.compute.internal,
-ip-172-21-110-185.us-west-2.compute.internal,  not cert-manager-webhook.cert-manager.svc
+certificate is valid for <fargate ip 1>, <fargate ip 2>,  not cert-manager-webhook.cert-manager.svc
